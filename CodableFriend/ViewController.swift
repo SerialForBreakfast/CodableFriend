1e9ef3e025288025fbc30a044ed0c60b45ed0870
Joe McCraw <joe@showblender.com>
2019-11-25 10:55:30 -0800

    Added search results.

--- CodableFriend/ViewController.swift
+++ CodableFriend/ViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UISearchResultsUpdating {
+    
+    
     var friends = [Friend]()
+    var filteredFriends = [Friend]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let search = UISearchController(searchResultsController: nil)
+        search.obscuresBackgroundDuringPresentation = false
+        search.searchBar.placeholder = "Find a friend"
+        search.searchResultsUpdater = self
+        navigationItem.searchController = search
+        
+        
         DispatchQueue.global().async {
             do {
                 let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
@@ -23,6 +34,7 @@ class ViewController: UITableViewController {
                 let downloadedFriends = try decoder.decode([Friend].self, from: data)
                 DispatchQueue.main.async {
                     self.friends = downloadedFriends
+                    self.filteredFriends = downloadedFriends
                     self.tableView.reloadData()
                     print(self.friends)
                 }
@@ -32,14 +44,26 @@ class ViewController: UITableViewController {
         }
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friends.count
+        return filteredFriends.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let friend = friends[indexPath.row]
+        let friend = filteredFriends[indexPath.row]
         cell.textLabel?.text = friend.name
         cell.detailTextLabel?.text = friend.friends.map{$0.name }.joined(separator: ", ")
         return cell
+    }
+    func updateSearchResults(for searchController: UISearchController) {
+        if let text = searchController.searchBar.text, text.count > 0 {
+            filteredFriends = friends.filter{
+                $0.name.contains(text)
+                    || $0.company.contains(text)
+                || $0.address.contains(text)
+            }
+        } else {
+            filteredFriends = friends
+        }
+        tableView.reloadData()
     }
 }
 
