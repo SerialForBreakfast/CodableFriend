c907d97934eced1b12065674bf2c64136317fabb
Joe McCraw <joe@showblender.com>
2019-11-25 09:12:53 -0800

    Build Friends and Connection structs

---
+++ CodableFriend/Connection.swift
@@ -0,0 +1,14 @@
+//
+//  Connection.swift
+//  CodableFriend
+//
+//  Created by Joseph McCraw on 11/25/19.
+//  Copyright © 2019 Joseph McCraw. All rights reserved.
+//
+
+import Foundation
+
+struct Connection: Codable {
+    var id: UUID
+    var name: String
+}
---
+++ CodableFriend/Friend.swift
@@ -0,0 +1,25 @@
+//
+//  Friend.swift
+//  CodableFriend
+//
+//  Created by Joseph McCraw on 11/25/19.
+//  Copyright © 2019 Joseph McCraw. All rights reserved.
+//
+
+import Foundation
+
+struct Friend: Codable {
+    var id: UUID
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [Connection]
+    
+}
+
